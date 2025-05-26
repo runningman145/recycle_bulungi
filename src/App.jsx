80ed7abc5b1c77c0ac69c3bc80f191f1c991b329
Kokole Wahid <wkabdul@gmail.com>
2025-05-26 07:29:35 +0300

    color conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,18 @@ import outputs from "../amplify_outputs.json";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix leaflet's default icon path so markers show up
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 import { 
   Recycle, 
